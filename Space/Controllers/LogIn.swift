80e58a0afad50a6a1e660cc617931e9af6457d23
Nilrajsinh <Nilrajsinh1@icloud.com>
2020-04-25 16:34:40 +0530

    Sign In with Apple Done

--- Space/Controllers/LogIn.swift
+++ Space/Controllers/LogIn.swift
@@ -9,60 +9,80 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
 
-class LogIn: UIViewController,GIDSignInDelegate {
+class LogIn: UIViewController
+, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
     
-       func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    fileprivate var currentNonce: String?
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         
-        
-        
-            if let err = error{
-                print("Fail To login",err)
-                return
-            }
-                print("Successfully login Done")
-              guard let authentication = user.authentication else { return }
-
-            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-
-            Auth.auth().signIn(with: credential) { (user, error) in
-                if let err = error {
-                    print("Error")
-                }
-
-
-                appDelegate.loginUserID = user?.user.uid ?? ""
-                self.performSegue(withIdentifier: "Home", sender: nil)
-//                let vc = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "Home")
-//                let nav = UINavigationController(rootViewController: vc)
-
-                print("Done")
-            }
-
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+            return
         }
+        
+         
+        
+        guard let nonce = currentNonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+
+        // Retrieve Apple identity token
+        guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Failed to fetch identity token")
+            return
+        }
+
+        // Convert Apple identity token to string
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Failed to decode identity token")
+            return
+        }
+
+        // Initialize a Firebase credential using secure nonce and Apple identity token
+        let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                          idToken: idTokenString,
+                                                          rawNonce: nonce)
+        Auth.auth().signIn(with: firebaseCredential) { [weak self] (authResult, error) in
+            // Do something after Firebase sign in completed
+            UserDefaults.standard.set(true, forKey: "ISUSERLOGGEDIN")
+                      UserDefaults.standard.synchronize()
+                      
+                       appDelegate.loginUserID = authResult?.user.uid ?? ""
+            self!.performSegue(withIdentifier: "Home", sender: nil)
+            print("Ohh")
+        }
+        
+        
+        print("appleid Credentail:\(appleIDCredential.user)")
+        
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("AppleID Credential failed with error: \(error.localizedDescription)")
+    }
+    
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+         return self.view.window!
+    }
 
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.setupSignInButton()
+    
+    
+    
          let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
                      
                      view.addGestureRecognizer(tap)
 
-//        let googlebutton = GIDSignInButton()
-//        googlebutton.layer.cornerRadius = 20
-//        googlebutton.frame = CGRect(x: 16, y: 116 + 66, width: view.frame.width - 32, height: 50)
-//        view.addSubview(googlebutton)
-        
-        
-        
-        
-        
-        Login.layer.cornerRadius = 20
-        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-        GIDSignIn.sharedInstance().delegate = self
-       GIDSignIn.sharedInstance()?.presentingViewController = self
+
+       
+      
         
 //        GIDSignIn.sharedInstance().signIn()
         
@@ -78,47 +98,116 @@ class LogIn: UIViewController,GIDSignInDelegate {
 
 
     
-    @IBAction func LoginBtn(_ sender: Any) {
-        
-        if Email.text == "" || Password.text == "" {
-            
-            GIDSignIn.sharedInstance()?.signIn()
-             UserDefaults.standard.set(true, forKey: "ISUSERLOGGEDIN")
-            UserDefaults.standard.synchronize()
-//              let alert = UIAlertController(title: "Oops", message: "Kindly login through google sign in", preferredStyle: .alert)
-//                                           let restartAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
-//                                           alert.addAction(restartAction)
-//                                           present(alert, animated: true, completion: nil)
-            
-            
-        }
-        else {
-        Auth.auth().signIn(withEmail: Email.text!, password: Password.text!) { (user, error) in
-            if error != nil {
-              
-                           GIDSignIn.sharedInstance()?.signIn()
-                            UserDefaults.standard.set(true, forKey: "ISUSERLOGGEDIN")
-                           UserDefaults.standard.synchronize()
-            }
-            
-             UserDefaults.standard.set(true, forKey: "ISUSERLOGGEDIN")
-            UserDefaults.standard.synchronize()
-            
-             appDelegate.loginUserID = user?.user.uid ?? ""
-            self.performSegue(withIdentifier: "Home", sender: nil)
-        }
-        }
-    }
-    @IBOutlet weak var Login: UIButton!
-    @IBOutlet weak var Email: UITextField!
-    @IBOutlet weak var Password: UITextField!
-    
+  
     
     @objc func dismissKeyboard() {
                   view.endEditing(true)
                  
               }
     
+    
+    
+    
+    
+    private func setupSignInButton() {
+        let signInButton = ASAuthorizationAppleIDButton(type: .default, style: .black)
+        signInButton.cornerRadius = 20
+           signInButton.addTarget(self, action: #selector(LogIn.signInButtonTapped), for: .touchDown)
+           
+           signInButton.translatesAutoresizingMaskIntoConstraints = false
+           self.view.addSubview(signInButton)
+           
+           NSLayoutConstraint.activate([
+            
+            
+            NSLayoutConstraint(item: signInButton, attribute: .bottom, relatedBy: .equal, toItem: bottomLayoutGuide, attribute: .top, multiplier: 1, constant: -150),
+            NSLayoutConstraint(item: signInButton,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1,
+            constant: 0),
+            
+               signInButton.heightAnchor.constraint(equalToConstant: 50),
+               
+               signInButton.widthAnchor.constraint(equalToConstant: 250)
+           ])
+      
+       }
+    
+   
+    
+    
+    @objc private func signInButtonTapped() {
+        startSignInWithAppleFlow()
+        let authorizationProvider = ASAuthorizationAppleIDProvider()
+        let request = authorizationProvider.createRequest()
+        request.requestedScopes = [.email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+ private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
 
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
+    }
+
+
+
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+
+    @available(iOS 13, *)
+    func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
 }
 
